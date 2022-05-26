b3d9a4d8964e7c1fe80fb172f006132086381561
Yevhen-Synehin <93155990+Yevhen-Synehin@users.noreply.github.com>
2022-05-26 21:07:23 +0000

    Deploying to gh-pages from @ Yevhen-Synehin/goit-js-hw-09@08c58107e0889e2427f5c309aa454a42a50b6e58 🚀

---
+++ 01-color-switcher.cbbd69f1.js
@@ -0,0 +1,2 @@
+var bodyRef=document.querySelector("body"),startBtn=document.querySelector("[data-start]"),stopBtn=document.querySelector("[data-stop]");function getRandomHexColor(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}
+//# sourceMappingURL=01-color-switcher.cbbd69f1.js.map
